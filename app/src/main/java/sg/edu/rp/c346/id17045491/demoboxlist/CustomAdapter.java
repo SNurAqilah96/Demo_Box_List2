fcf830fae230c52f566b12f5532af577c0390a49
SITI NUR AQILAH BTE ISMAIL RP <17045491@myrp.edu.sg>
2020-07-28 21:16:42 +0800

    update

---
+++ app/src/main/java/sg/edu/rp/c346/id17045491/demoboxlist/CustomAdapter.java
@@ -0,0 +1,47 @@
+package sg.edu.rp.c346.id17045491.demoboxlist;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+public class CustomAdapter extends ArrayAdapter {
+    Context parent_context;
+    int layout_id;
+    ArrayList<Box> boxList;
+
+    public CustomAdapter(Context context,int resource,ArrayList<Box>objects){
+        super(context,resource,objects); // calling parent constructor to call these things
+        parent_context=context;
+        layout_id=resource;
+        boxList=objects;
+
+    }
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        LayoutInflater inflater = (LayoutInflater) parent_context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        View rowView = inflater.inflate(layout_id, parent, false);
+
+
+        ImageView ivBox = rowView.findViewById(R.id.imageViewBox);
+
+        Box currentItem = boxList.get(position);
+
+        if(currentItem.getColour().equals("blue")){
+            ivBox.setImageResource(R.drawable.blue_box);
+        }
+        else if(currentItem.getColour().equals("orange")){
+            ivBox.setImageResource(R.drawable.orange_box);
+        }
+        else {
+            ivBox.setImageResource(R.drawable.brown_box);
+        }
+        return rowView;
+
+    }
+}
